5d47a6f004d3db46160e685ab93a9cd3d541e82b
Delphine Le Brun <delphine.clebrun@gmail.com>
2021-01-13 15:11:40 +0100

    prog bloc

--- coockeat/src/components/homepage/HomePage.jsx
+++ coockeat/src/components/homepage/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Banner from "./Banner";
+import Prog from "./Prog";
 
 const HomePage = () => {
   return (
     <div>
       <Banner />
+      <Prog />
     </div>
   );
 };
