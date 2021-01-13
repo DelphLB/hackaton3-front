e398e86b1821c13fd278a4bef31016ccfce0814e
Delphine Le Brun <delphine.clebrun@gmail.com>
2021-01-14 00:42:58 +0100

    navbar + css

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import HomePage from "./components/homepage/HomePage";
 import { Route, Switch } from "react-router";
 import VideoChat from "./video/VideoChat";
 import "./App.css";
+import NavBar from "./navbar/NavBar";
 
 function App() {
   return (
     <div className='App'>
+      <NavBar />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/videoChat' component={VideoChat} />
