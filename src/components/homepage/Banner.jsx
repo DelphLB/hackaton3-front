5769b74266d6bc1a9c9fa06a76fd984785025776
jeyofdev <jgregoire.pro@gmail.com>
2021-01-14 23:12:15 +0100

    fix : fix banner.jsx

--- src/components/homepage/Banner.jsx
+++ src/components/homepage/Banner.jsx
@@ -3,11 +3,11 @@ import '../../style/Banner.css';
 
 function Banner() {
     return (
-      <div className="container-banner">
-          
-        <div className="box-name-projet">
-            <h1 className="cook-eat">Cook'eat</h1>
-            <p className="slogan"> cook it, live it, eat it </p>
+        <div className="container-banner">
+            <div className="box-name-projet">
+                <h1 className="cook-eat">Cook'eat</h1>
+                <p className="slogan"> cook it, live it, eat it </p>
+            </div>
         </div>
     );
 }
