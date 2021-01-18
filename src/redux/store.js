fbd68d87a24b59681192ad2d6cab4bb0ef5d0062
RomainMouly <romainmouly@gmail.com>
2021-01-18 17:45:56 +0100

    add redux-persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,26 @@
-import { createStore, compose, combineReducers } from 'redux';
-import userReducer from './reducers/userReducer';
+import { createStore, compose, combineReducers } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import userReducer from "./reducers/userReducer";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
 
 const rootReducers = combineReducers({
-    user: userReducer,
+  user: userReducer,
 });
 
-const store = createStore(
-    rootReducers,
-    compose(
-        window.__REDUX_DEVTOOLS_EXTENSION__ &&
-            window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
+const persistedReducer = persistReducer(persistConfig, rootReducers);
+
+export const store = createStore(
+  persistedReducer,
+  compose(
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 );
 
-export default store;
+export const persistor = persistStore(store);
+
+export default { store, persistor };
